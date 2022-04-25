c94a57de37df19f56eb67d4a14b17f1522ae73a1
konarsada <sadabratak@gmail.com>
2022-04-26 01:18:21 +0530

    project 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from "./data"
+
+import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
+export default function App() {
+    const cards = data.map(item => (
+        <Card
+            key={item.id}
+            {...item}
+        />
+    ))
+
+    return (
+        <div>
+            <NavBar />
+            <Hero />
+            <section className="cards-list">
+                {cards}
+            </section>
+        </div>
+    )
 }
-
-export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function NavBar() {
+    return (
+        <nav>
+            <img src="../images/airbnb-logo.png"
+                alt="airbnb--logo"
+                className="airbnb--logo"
+            />
+        </nav>
+    )
+}
